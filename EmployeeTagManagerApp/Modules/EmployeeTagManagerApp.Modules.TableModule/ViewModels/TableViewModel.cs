c70cabbe8f3fde1c6a9ac6115d5696667209990f
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-07-22 14:55:53 +0200

    Add possibility to delete employee with confirmation message box

--- EmployeeTagManagerApp/Modules/EmployeeTagManagerApp.Modules.TableModule/ViewModels/TableViewModel.cs
+++ EmployeeTagManagerApp/Modules/EmployeeTagManagerApp.Modules.TableModule/ViewModels/TableViewModel.cs
@@ -71,11 +71,18 @@ namespace EmployeeTagManagerApp.Modules.TableModule.ViewModels
                 }
             });
         }
-
-
-        private void DeleteEmployee(Employee employee)
+        private async void DeleteEmployee(Employee employee)
         {
-            // Logic for deleting the employee
+            if (employee == null) return;
+            var result = MessageBox.Show("Are you sure you want to delete this employee?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                await _employeeService.DeleteEmployeeAsync(employee.Id);
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Employees.Remove(employee);
+                });
+            }
         }
     }
 
